eba8a8aa9bc896ee47ca75c93fd1d3378a0f6487
alex <alekceytr@mail.ru>
2018-11-17 12:08:42 +0800

    test

--- app/src/main/java/com/example/aleks/labsrv/MainActivity.java
+++ app/src/main/java/com/example/aleks/labsrv/MainActivity.java
@@ -2,12 +2,28 @@ package com.example.aleks.labsrv;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText editText;
+    Button button;
+    TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        editText = findViewById(R.id.editText);
+        button = findViewById(R.id.button);
+        textView = findViewById(R.id.textView5);
+
+    }
+
+    public void calculate(View view) {
+        textView.setText(editText.getText());
     }
 }
